c9003d562abf181f16b49ad67608357180b7d209
Hieuto0409 <kid074334@gmail.com>
2022-12-04 09:57:51 +0700

    first commit SizeBar

--- src/components/MenuSizeBar/MenuItems.js
+++ src/components/MenuSizeBar/MenuItems.js
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-function MenuItems({ icon, to, title }) {
+import clsx from 'clsx';
+import style from './style.module.css';
+import { useState } from 'react';
+function MenuItems({ icon, to, title, className, activeIcon }) {
+    const [CheckActiveIcon, setNone] = useState(false);
     return (
         <div>
-            <NavLink to={to}>
-                {title}
-                <span>{icon}</span>
+            <NavLink
+                to={to}
+                className={(value) => {
+                    value.isActive ? setNone(true) : setNone(false);
+                    return clsx(className, style.ItemsHover, { [style.active]: value.isActive });
+                }}
+            >
+                <span className={clsx({ [style.None]: CheckActiveIcon })}>{icon}</span>
+                <span className={clsx(style.None, { [style.Show]: CheckActiveIcon })}>{activeIcon}</span>
+                <span className={style.Title}>{title}</span>
             </NavLink>
         </div>
     );
@@ -14,5 +25,6 @@ MenuItems.prototypes = {
     icon: PropTypes.node.isRequired,
     title: PropTypes.string.isRequired,
     to: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired,
 };
 export default MenuItems;
--- src/layouts/LayoutDefault/Sizebar/Sizebar.js
+++ src/layouts/LayoutDefault/Sizebar/Sizebar.js
@@ -2,13 +2,33 @@ import style from './SizeBar.module.css';
 import clsx from 'clsx';
 import Menu, { MenuItems } from '~/components/MenuSizeBar';
 import Config from '~/routes/Routes';
+import { LiveIcon, LiveIconActive, FlowingIcon, FlowingIconActive, HomeIcon, HomeIconActive } from '~/components/icon';
 function SizeBar() {
+    // đang active icon
     return (
         <div className={clsx(style.container)}>
             <Menu>
-                <MenuItems to={'/'} title={'For you'} icon={null} />
-                <MenuItems to={Config.Routes.Following} title={'Flowing'} icon={null} />
-                <MenuItems to={Config.Routes.Live} title={'Live'} icon={null} />
+                <MenuItems
+                    className={clsx(style.Menuitems)}
+                    to={'/'}
+                    title={'For you'}
+                    icon={<HomeIcon />}
+                    activeIcon={<HomeIconActive />}
+                />
+                <MenuItems
+                    className={clsx(style.Menuitems)}
+                    to={Config.Routes.Following}
+                    title={'Flowing'}
+                    icon={<FlowingIcon />}
+                    activeIcon={<FlowingIconActive />}
+                />
+                <MenuItems
+                    className={clsx(style.Menuitems)}
+                    to={Config.Routes.Live}
+                    title={'Live'}
+                    icon={<LiveIcon />}
+                    activeIcon={<LiveIconActive />}
+                />
             </Menu>
         </div>
     );
